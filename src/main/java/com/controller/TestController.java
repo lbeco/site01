ede843ef77d129a9a817c62196e65995ad4a9d47
linbc <linbc@zju.edu.cn>
2021-06-07 23:52:03 +0800

    redirect

--- src/main/java/com/controller/TestController.java
+++ src/main/java/com/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.controller;
 
 import com.service.impl.TestServiceImpl;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
 
 @Controller
 //@RequiredArgsConstructor
+@RequestMapping("/test")
 public class TestController {
     @Autowired
     private final TestServiceImpl testService;
@@ -17,9 +25,17 @@ public class TestController {
     }
 
     @ResponseBody
-    @RequestMapping("/test")
-    public String test(){
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
         return testService.test();
+    }
+
+    @ApiOperation("重定向")
+    @RequestMapping(value = "redirect", method = RequestMethod.GET)
+    @ResponseBody
+    public void testRedirect(HttpServletRequest request, HttpServletResponse response, Writer out)
+            throws IOException {
+        response.sendRedirect("/user/getUser/3");
     }
 
 }
